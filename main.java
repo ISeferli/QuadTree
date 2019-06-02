297ea20611ddfa97c91d16cfaad9d330aa94d75c
ISeferli <50150351+ISeferli@users.noreply.github.com>
2019-06-02 20:28:33 +0300

    Update main.java

--- main.java
+++ main.java
@@ -17,27 +17,27 @@ public class Main {
 	public static void main(String[] args) {
 /*Each time for different M number of nodes we make a different tree with the help of kdtreeMainHelp() function 
   where we take the number M and use it over and over again to make the tree and then search for different values */
-     
-          System.out.println("-------------------QUAD TREE--------------------");
-            //For M>50000 sometimes it has stack overflow errors and so we use N = 2^19, KD Tree works fine with N = 2^16
-          M = 1000; 
-          helper = new Point2D[M];
-          quadTreeHelp(M);
-          M = 10000;
-        helper = new Point2D[M];
-            quadTreeHelp(M);
-            M = 30000;
-        helper = new Point2D[M];
-            quadTreeHelp(M);
-            M = 50000;
-        helper = new Point2D[M];
-            quadTreeHelp(M);
-            M = 70000;		
-            helper = new Point2D[M];
-            quadTreeHelp(M);
-            M = 100000;
-        helper = new Point2D[M];
-            quadTreeHelp(M);	
+
+		  System.out.println("-------------------QUAD TREE--------------------");
+	 //For M>50000 sometimes it has stack overflow errors and so we use N = 2^19, KD Tree works fine with N = 2^16
+		  M = 1000; 
+		  helper = new Point2D[M];
+		  quadTreeHelp(M);
+		  M = 10000;
+		 helper = new Point2D[M];
+		 quadTreeHelp(M);
+		 M = 30000;
+		helper = new Point2D[M];
+		quadTreeHelp(M);
+		M = 50000;
+		helper = new Point2D[M];
+		quadTreeHelp(M);
+		M = 70000;		
+		helper = new Point2D[M];
+		quadTreeHelp(M);
+		M = 100000;
+		helper = new Point2D[M];
+		quadTreeHelp(M);	
 
 	}	
   
@@ -45,12 +45,12 @@ public class Main {
   
 	//Method that we use to create a random point and not write the same code again and again
 	public static Point2D createPointMain() {		
-        Random rand = new Random();
+		Random rand = new Random();
 
 		x = rand.nextInt(N);  
-    	y = rand.nextInt(N);
-    	point = new Point2D(x, y);
-    	return point;
+		y = rand.nextInt(N);
+		point = new Point2D(x, y);
+		return point;
 	}
 	
   
@@ -68,38 +68,38 @@ public class Main {
 			qTree.insert(point, value, N);
 		}
 		
-	      System.out.println("For M=" +M);
+	        System.out.println("For M=" +M);
 	        System.out.println("Elements that exist in the tree");
 	    	boolean check;
 	    	double accesses=0;
 	    	int k = 0;
 	    	
-			while(qTree.getSuccesfullSearch()<searches) {
-				point = helper[k];
-				qTree.setNumberOfAccesses(0);
-				check = qTree.searchQuad(qTree.getRoot(), point);
-				
-				if(check==true) {
-	        		accesses += qTree.getNumberOfAccesses();
-	        		k++;
-	        	}else if(check==false){
-	        		accesses += 0;
-	        	}
+		while(qTree.getSuccesfullSearch()<searches) {
+			point = helper[k];
+			qTree.setNumberOfAccesses(0);
+			check = qTree.searchQuad(qTree.getRoot(), point);
+
+			if(check==true) {
+				accesses += qTree.getNumberOfAccesses();
+				k++;
+			}else if(check==false){
+				accesses += 0;
 			}
+		}
 	        System.out.println("Average number of successful accesses: "+accesses/searches);
 
 	        qTree.setUnsuccesfullSearch(0);
-			while(qTree.getUnsuccesfullSearch()<searches) {
+		while(qTree.getUnsuccesfullSearch()<searches) {
 			point = createPointMain();
 			qTree.setNumberOfAccesses(0);
-        	check = qTree.searchQuad(qTree.getRoot(), point);
-        	
-        	if(check==true) {
-        		accesses += 0;
-        	}else if(check==false){
-        		accesses += qTree.getNumberOfAccesses();
-        	}
+			check = qTree.searchQuad(qTree.getRoot(), point);
+
+			if(check==true) {
+				accesses += 0;
+			}else if(check==false){
+				accesses += qTree.getNumberOfAccesses();
+			}
 		}
 		
-        System.out.println("Average number of unsuccessful accesses: "+accesses/searches);
-		}
+        	System.out.println("Average number of unsuccessful accesses: "+accesses/searches);
+	}
